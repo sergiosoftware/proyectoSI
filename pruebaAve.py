f2ddb29b1234a97f6fda5c964c021b7924fdc1c2
sergiosoftware <sergio95andres@gmail.com>
2020-01-19 00:29:30 -0500

    inicial

---
+++ pruebaAve.py
@@ -0,0 +1,18 @@
+import cv2
+
+from prediccion import prediccion
+
+categorias=["Gaviota occidental","Gallinas","Coragyps atratus (Gallinazo)","Tacuarita","Mirlo de alas rojas",
+ "green violetear","Cardenal norteño","Hapalopsittaca amazonina","scarlet tanager",
+ "momoto amazónico (barranquillo coronado)","Yellow headed Blackbird(Mirlo de cabeza amarilla)","Lazuli Bunting"]
+#categorias=["1","2","3","4","5","6","7","8","9","10","11","12"]
+reconocimiento=prediccion()
+imagenPrueba=cv2.imread("test/12/12_12_4.jpg",0)
+indiceCategoria=reconocimiento.predecir(imagenPrueba)
+print("La imagen cargada es ",categorias[indiceCategoria-1])
+while True:
+    cv2.imshow("imagen",imagenPrueba)
+    k=cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+cv2.destroyAllWindows()
